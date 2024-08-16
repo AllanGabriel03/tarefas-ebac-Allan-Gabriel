4e8f49059b7dc15bd348d3c14808699fc52e8a63
AllanGabriel03 <147627527+AllanGabriel03@users.noreply.github.com>
2024-08-16 10:06:33 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,16 @@
+package objeto;
+
+public class Cliente {
+
+	private String nome;
+	
+	/**
+	 * Método que retorna nome do cliente 
+	 * @return retorna o nome 
+	 */
+	
+    public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
