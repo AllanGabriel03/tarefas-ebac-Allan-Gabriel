8d8e8e6984d6f542765282c9ce5b45c6e67c766b
Allan <allan.gabriel587@gmail.com>
2024-08-20 09:20:19 -0300

    CalculoMédia

---
+++ calculoMedia.java
@@ -0,0 +1,17 @@
+package objeto;
+
+public class calculoMedia {
+
+	public static void main(String[] args) {
+		double num1 = 5;
+		double num2 = 9;
+		double num3 = 2;
+		double num4 = 14;
+		
+		double media = (num1 + num2 + num3 + num4) / 4;
+		
+		System.out.print("Média: " + media);
+
+	}
+
+}
