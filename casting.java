a5e3915536858da1fdeff6fdec0174dda3e53b47
Allan <allan.gabriel587@gmail.com>
2024-08-27 06:26:36 -0300

    Subindo tarefa de casting

---
+++ casting.java
@@ -0,0 +1,15 @@
+package primitivo;
+
+public class casting {
+
+	public static void main(String[] args) {
+		int i1 = 1111;
+		System.out.println("int: " + i1);
+		
+		Integer i2 = i1; 
+		System.out.print("Integer: " + i2);
+		
+				
+	}
+
+}
