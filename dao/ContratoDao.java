ccfee3499c25a914d65f419f0182c8d8342ec1db
Allan <allan.gabriel587@gmail.com>
2024-11-20 10:45:13 -0300

    Subindo tarefas

---
+++ dao/ContratoDao.java
@@ -0,0 +1,23 @@
+package dao;
+
+public class ContratoDao implements IContratoDao{
+    @Override
+    public void salvar() {
+        throw new UnsupportedOperationException("Não funciona com o banco");
+    }
+
+    @Override
+    public void buscar() {
+
+    }
+
+    @Override
+    public void atualizar() {
+
+    }
+
+    @Override
+    public void excluir() {
+
+    }
+}
