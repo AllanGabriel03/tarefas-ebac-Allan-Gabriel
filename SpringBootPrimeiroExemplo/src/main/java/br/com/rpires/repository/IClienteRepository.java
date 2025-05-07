9942c52b50ec1b56e81407d032c97b91f3238729
Allan <allan.gabriel587@gmail.com>
2025-05-07 09:47:05 -0300

    subindo branch

---
+++ SpringBootPrimeiroExemplo/src/main/java/br/com/rpires/repository/IClienteRepository.java
@@ -0,0 +1,11 @@
+package br.com.rpires.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.rpires.domain.Cliente;
+
+@Repository
+public interface IClienteRepository extends CrudRepository<Cliente, Long>{
+
+}
